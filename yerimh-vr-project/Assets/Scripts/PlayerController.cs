61c76b913dcba65712b0a24fa6a23df6a13e9726
Yerim Heo <yerimh@uw.edu>
2019-10-30 02:17:39 -0700

    can't figure out scripting

---
+++ yerimh-vr-project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float speed = 5;
+	public Rigidbody rigid;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+			rigid.MovePosition(transform.position + this.transform.forward * speed * Time.deltaTime);
+		} else if (Input.GetKey(KeyCode.S)) {
+			rigid.MovePosition(transform.position + -this.transform.forward * speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.A)) {
+			rigid.MovePosition(transform.position + -transform.right * speed * Time.deltaTime);
+		} else if (Input.GetKey(KeyCode.D)) {
+			rigid.MovePosition(transform.position + transform.right * speed * Time.deltaTime);
+		}
+    }
+}
